025596eb807648495237c7468bbe559884fa32f0
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-05-24 20:28:24 +0700

    create Box and BoxesContainer components

---
+++ src/Box.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { choice } from './helpers';
+import './Box.css';
+
+class Box extends Component {
+  static defaultProps = {
+    allColors: ['purple', 'magenta', 'violet', 'pink'],
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: choice(this.props.allColors),
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  pickColor() {
+    let newColor = choice(this.props.allColors);
+    do {
+      newColor = choice(this.props.allColors);
+    } while (newColor === this.state.color);
+
+    this.setState({ color: newColor });
+  }
+
+  handleClick() {
+    this.pickColor();
+  }
+
+  render() {
+    return (
+      <div
+        className="Box"
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}
+      ></div>
+    );
+  }
+}
+
+export default Box;
---
+++ src/BoxesContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import './BoxesContainer.css';
+
+class BoxesContainer extends Component {
+  static defaultProps = {
+    numBoxes: 18,
+    // allColors: ['purple', 'magenta', 'violet', 'pink'],
+  };
+
+  render() {
+    const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
+      <Box />
+    ));
+    return <div className="BoxesContainer">{boxes}</div>;
+  }
+}
+
+export default BoxesContainer;
